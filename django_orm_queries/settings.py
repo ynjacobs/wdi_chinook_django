0c97e053ef89ba48bfb7895a70d591602966cf06
NatalieBlack <natalievictoriablack@gmail.com>
2019-01-19 20:12:13 -0500

    Move chinook app up a level and switch to psql

--- django_orm_queries/settings.py
+++ django_orm_queries/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_orm_queries.chinook',
+    'chinook.apps.ChinookConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'django_orm_queries.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chinook',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
